d8abc8b0d77cf95cd953170511daee8e32ce1c98
sp2254 <sp2254@njit.edu>
2017-05-03 14:41:47 -0400

    login page created

---
+++ view/login.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<h1> This is a login page</h1>
+<form method ="post" action="index.php">
+<strong>Username:</strong><input type="text" name="username" value=""/><br><br>
+<strong>Password:</strong><input type="password" name="password" value=""/><br><br>
+<input type="hidden" name="action" value="login_user"/><br>
+<input type="submit" value="Login"/>
+</form>
+<form action= "register.php">
+ <input type="submit" value="Sign Up"/>
+ </form>
+ </body>
+ </html>
+ 
